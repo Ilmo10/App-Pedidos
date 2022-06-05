257428b2bdcb9f16dc90426c359dc7f739f044c8
Ilmo10 <necok_cley@hotmail.com>
2022-06-05 14:38:33 -0300

    Criando API

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.get('/:nome/:idade',function(recebendo, respondendo){
 
-
-
-
-
-
-
+    const nome = recebendo.params.nome
+    const idade = recebendo.params.idade
+    console.log(nome)
+    respondendo.json({
+        Nome: nome,
+        idade: idade
+    })
+})
 
 app.listen(1010, function(req,res){
     console.log('O server subiu!!')
