aa59af0f47a7c78bdda070e3004faa9a9a874417
Ilmo10 <necok_cley@hotmail.com>
2022-06-05 23:50:41 -0300

    somando total produto e incluindo no total. Estudo sobre API, Servidor e front. finalizado.

--- api/produtos.js
+++ api/produtos.js
@@ -14,16 +14,100 @@
 },
 {
   id: 3,
-  name: 'Coca Cola',
-  description: 'Bebida ',
-  type: 2,
-  value: 400
+  name: 'coxinha',
+  description: 'salgado frito',
+  type: 1,
+  value: 0.5
 },
 {
   id: 4,
+  name: 'espetinho',
+  description: 'salgado frito',
+  type: 1,
+  value: 0.5
+},
+{
+  id: 5,
+  name: 'Pastel queijo',
+  description: 'salgado frito',
+  type: 1,
+  value: 0.5
+},
+{
+  id: 6,
+  name: 'bolinha de queijo',
+  description: 'salgado frito',
+  type: 1,
+  value: 0.5
+},
+{
+  id: 7,
+  name: 'kibe',
+  description: 'salgado frito',
+  type: 1,
+  value: 0.5
+},
+{
+  id: 8,
+  name: 'risole carne',
+  description: 'salgado frito',
+  type: 1,
+  value: 0.5
+},
+{
+  id: 9,
+  name: 'risole pm',
+  description: 'salgado frito',
+  type: 1,
+  value: 0.5
+},
+{
+  id: 1.0,
+  name: 'Coca Cola',
+  description: 'Bebida ',
+  type: 2,
+  value: 4
+},
+{
+  id: 1.1,
   name: 'Fanta',
   description: 'bebida',
   type: 2,
-  value: 400
-}] 
+  value: 4
+},
+{
+  id: 1.2,
+  name: 'sprite',
+  description: 'Bebida ',
+  type: 2,
+  value: 4
+},
+{
+  id: 1.3,
+  name: 'guarana',
+  description: 'Bebida ',
+  type: 2,
+  value: 4
+},
+{
+  id: 1.4,
+  name: 'jesus',
+  description: 'Bebida ',
+  type: 2,
+  value: 4
+},
+{
+  id: 1.5,
+  name: 'tampico',
+  description: 'Bebida ',
+  type: 2,
+  value: 4
+},
+{
+  id: 1.6,
+  name: 'água',
+  description: 'Bebida ',
+  type: 2,
+  value: 4
+},] 
 module.exports = {produto}
--- script.js
+++ script.js
@@ -7,6 +7,7 @@ fetch('http://192.168.100.178:1010/produto') // http://localhost:1010/produto
     listaProdutos(data)
   })
   .catch(function(){
+    listaProdutos(produtoarr())
       console.log('deu ruim')
   })
   function listaProdutos(data){
@@ -30,7 +31,7 @@ fetch('http://192.168.100.178:1010/produto') // http://localhost:1010/produto
           <button onclick="subtrai(${produto.id})">-</button>
         </div>
         <div class="quantidade">
-          <span id="quantidade${produto.id}" data-value="${produto.value}">0</span>
+          <span class="quant" id="quantidade${produto.id}" data-type="${produto.type}" data-value="${produto.value}">0</span>
         </div>
         <div class="mais">
           <button onclick="soma(${produto.id})">+</button>
@@ -42,33 +43,157 @@ fetch('http://192.168.100.178:1010/produto') // http://localhost:1010/produto
 
   function subtrai(id){
     const elQuantidade = document.getElementById('quantidade' + id)
-    let menos = parseInt(elQuantidade.innerText) - 25
+    const type = elQuantidade.dataset.type
+    let quant = type == '1' ? 25 : 1
+    let menos = parseInt(elQuantidade.innerText) - quant
     if(parseInt(elQuantidade.innerText)<= 0){
       menos = 0
     }
     elQuantidade.innerHTML = menos
-    total(id)
+    total()
   }
   
   function soma(id){
     const elQuantidade = document.getElementById('quantidade' + id)
-    let soma = parseInt(elQuantidade.innerText) + 25
-    
+    const type = elQuantidade.dataset.type
+    let quant = type == '1' ? 25 : 1
+    let soma = parseInt(elQuantidade.innerText) + quant
+
+    console.log(type)
     elQuantidade.innerHTML = soma
+
     
     
-    total(soma,elQuantidade.dataset.value.id)
+    total()
   }
 
-  function total(soma,value,id){
-    value = parseFloat(value)
-    const elValue = document.getElementById('value')
-    let resultado = parseInt(elValue.innerText) + (soma * value)
-    
-    //elValue.innerHTML = resultado.toLocaleString('pt-br', {minimumFractionDigits: 2})
-    
+  function total(){
+    const elQuant = document.querySelectorAll('.quant')
+    let totalItens = [...elQuant]
+    let resultado = 0
+    for(elemento of totalItens){
+      resultado += parseFloat(elemento.dataset.value) * parseInt(elemento.innerText)
+      
+    }
+
+    let totalValue = document.getElementById('value')
+    totalValue.innerHTML = resultado.toLocaleString('pt-br', {minimumFractionDigits: 2})
+ 
   }
 
-
+function produtoarr(){
+  return [{
+    id: 1,
+    name: 'Pastel carne',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 2,
+    name: 'Pastel queijo',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 3,
+    name: 'coxinha',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 4,
+    name: 'espetinho',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 5,
+    name: 'Pastel queijo',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 6,
+    name: 'bolinha de queijo',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 7,
+    name: 'kibe',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 8,
+    name: 'risole carne',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 9,
+    name: 'risole pm',
+    description: 'salgado frito',
+    type: 1,
+    value: 0.5
+  },
+  {
+    id: 1.0,
+    name: 'Coca Cola',
+    description: 'Bebida ',
+    type: 2,
+    value: 4
+  },
+  {
+    id: 1.1,
+    name: 'Fanta',
+    description: 'bebida',
+    type: 2,
+    value: 4
+  },
+  {
+    id: 1.2,
+    name: 'sprite',
+    description: 'Bebida ',
+    type: 2,
+    value: 4
+  },
+  {
+    id: 1.3,
+    name: 'guarana',
+    description: 'Bebida ',
+    type: 2,
+    value: 4
+  },
+  {
+    id: 1.4,
+    name: 'jesus',
+    description: 'Bebida ',
+    type: 2,
+    value: 4
+  },
+  {
+    id: 1.5,
+    name: 'tampico',
+    description: 'Bebida ',
+    type: 2,
+    value: 4
+  },
+  {
+    id: 1.6,
+    name: 'água',
+    description: 'Bebida ',
+    type: 2,
+    value: 4
+  },] 
+}
   
 
